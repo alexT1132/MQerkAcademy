5c7dfe7f32a0a6be1087d379da74d9f148a9a480
Darian Reyes Romero <darianreyesromero@hotmail.com>
2025-03-06 09:41:44 -0600

    UX

--- client/src/pages/Colab/Documents/03-InfoProfes.jsx
+++ client/src/pages/Colab/Documents/03-InfoProfes.jsx
@@ -1,18 +1,20 @@
 import NavLogin from "../../../components/NavLogin"
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { FormGroup, FormControlLabel, Checkbox, TextField, FormControl, FormLabel, RadioGroup, Radio } from "@mui/material";
 
 
 function Bienvenida(){
     return(
-        <div>
-            <NavLogin/>
+        <html lang="es">
 
-            <div>          
+            <head>
+                <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+            </head>
+            <NavLogin/>
+            <body>
+            <main>
+            <article>          
                 <h1 className="instrucciones-reg">3. Información profesional</h1>
             
 
@@ -22,59 +24,147 @@ function Bienvenida(){
                 instituciones donde has trabajado, y áreas de especialización.
                 Esta información nos ayudará a evaluar mejor tu perfil como asesor.
                 </p>
-            </div>
-            <div>
+            </article>
+
+            <form className="cent-form"
+                style={{
+                    display:'flex',
+                    flexDirection:'column',
+                    alignItems:'stretch'
+                    }}>
                 {/* Área de interés o departamento al que aplicas:
-                    Administración
-                    Marketing
-                    Recursos humanos
-                    Tecnología
-                    Finanzas
-                    Logística
-                    Atención al cliente
-                    Ventas
-                    Otro (especificar)
-                    CHECKLIST
                     */}
+                <FormGroup
+                style={{
+                    'font-family':"Roboto,Helvetica,Arial,sans-serif",
+                    textAlign:'justify',
 
-                {/* Puesto de trabajo actual o último puesto
-                    TEXTFIELD */}
+                    }}>
+                        <h2>Área de interés o departamento al que aplicas</h2>
+                        <hr />
+                        <FormControlLabel control={<Checkbox />} label="Administración" />
+                        <FormControlLabel control={<Checkbox />} label="Marketing" />
+                        <FormControlLabel control={<Checkbox />} label="Recursos humanos" />
+                        <FormControlLabel control={<Checkbox />} label="Tecnología" />
+                        <FormControlLabel control={<Checkbox />} label="Finanzas" />
+                        <FormControlLabel control={<Checkbox />} label="Logística" />
+                        <FormControlLabel control={<Checkbox />} label="Atención al cliente" />
+                        <FormControlLabel control={<Checkbox />} label="Ventas" />
+                        <FormControlLabel control={<Checkbox />} label="Otro (especificar)" />
+                </FormGroup>
 
-                {/* Nombre de la empresa actual o anterior
-                    TEXTFIELD outlined-multiline-static*/}
-
-                {/* Funciones y responsabilidades en el puesto anterior: */}
                 
+                <fieldset>
+                    <h2>Informacion sobre anterior puesto/trabajo</h2>
+                    <hr />
+                    <TextField
+                        required
+                        label="Actual/último puesto de trabajo"
+                        variant="outlined"
+                        helperText='ej. Gerente general'
+                        multiline
+                        fullWidth
+                        style={{
+                            'margin-top':'15px',
+                            'margin-bottom':'25px'
+                        }}
+                    />
+                    
+                    <TextField
+                        required
+                        label="Empresa"
+                        variant="outlined"
+                        helperText='ej. GEGSA, Login360.'
+                        multiline
+                        fullWidth
+                        style={{
+                            'margin-bottom':'25px'
+                        }}
+                    />
+                    
 
-                {/* Experiencia laboral (años):
-                    Menos de 1 año
-                    1-2 años
-                    3-5 años
-                    6-10 años
-                    Más de 10 años
-                    CHECKLIST
-                    */}
+                    {/* Funciones y responsabilidades en el puesto anterior: */}
+                    <TextField
+                        required
+                        label="Funiones/responsabilidades"
+                        variant="outlined"
+                        helperText='Explica de forma detallada las funciones que realizabas en tu anterios puesto.'
+                        multiline
+                        rows={4}
+                        fullWidth
+                    />
 
-                {/* Herramientas o software que dominas:
-                    Microsoft Office (Word, Excel, PowerPoint)
-                    Google Workspace (Docs, Sheets, Slides)
-                    CRM (especificar cuál)
-                    Herramientas de marketing digital (Google Ads, Facebook Ads, etc.)
-                    Software de contabilidad (especificar)
-                    Otro (especificar)
-                    CHECKLIST
+                </fieldset>
+
+                <FormControl fullWidth>
+                    <FormLabel
+                        id="radio-buttons-group-label">
+                    </FormLabel>
+                        <h2>Experiencia laboral</h2>
+                        <hr />
+                        <RadioGroup
+                            row
+                            aria-labelledby="radio-buttons-group-label"
+                            name="radio-buttons-group"
+                            required
+                            style={{display:'flex', flexDirection:'column'}}
+                            >
+                    
+                                <FormControlLabel value="xp1" control={<Radio />} label="Menos de 1 año" />
+                                <FormControlLabel value="xp2" control={<Radio />} label="1-2 años" />
+                                <FormControlLabel value="xp3" control={<Radio />} label="3-5 años" />
+                                <FormControlLabel value="xp4" control={<Radio />} label="6-10 años" />
+                                <FormControlLabel value="xp5" control={<Radio />} label="Más de 10 años" />
+                                
+                        </RadioGroup>
+                </FormControl>
+                
+                {/* Herramientas o software que dominas
                     */}
+                <FormGroup
+                style={{
+                    'textAlign':'justify',
+                    'font-family':"Roboto,Helvetica,Arial,sans-serif",
+                    rowGap:'50%'
+                    }}>
+                        <h2>Herramientas o software que dominas</h2>
+                        <hr />
+                        <FormControlLabel control={<Checkbox />} label="Microsoft Office (Word, Excel, PowerPoint)" />
+                        <FormControlLabel control={<Checkbox />} label="Google Workspace (Docs, Sheets, Slides)" />
+                        <FormControlLabel control={<Checkbox />} label="CRM (especificar cuál)" />
+                        <FormControlLabel control={<Checkbox />} label="Herramientas de marketing digital (Google Ads, Facebook Ads, etc.)" />
+                        <FormControlLabel control={<Checkbox />} label="Software de contabilidad (especificar)" />
+                        <FormControlLabel control={<Checkbox />} label="Otro (especificar)" />
+                        
+                </FormGroup>
 
                 {/* Conocimientos o habilidades técnicas específicas (opcional):
                     Ejemplos: diseño gráfico, programación, análisis financiero, gestión de proyectos, etc.
                     TEXTFIELD
                     */}
+                <FormGroup
+                    helperText='Este apartado es opcional'
+                    style={{
+                    'font-family':"Roboto,Helvetica,Arial,sans-serif"
+                    }}>
+                        <h2>Conocimientos o habilidades técnicas específicas</h2>
+                        <hr />
+                        <FormControlLabel control={<Checkbox />} label="Microsoft Office (Word, Excel, PowerPoint)" />
+                        <FormControlLabel control={<Checkbox />} label="Google Workspace (Docs, Sheets, Slides)" />
+                        <FormControlLabel control={<Checkbox />} label="CRM (especificar cuál)" />
+                        <FormControlLabel control={<Checkbox />} label="Herramientas de marketing digital (Google Ads, Facebook Ads, etc.)" />
+                        <FormControlLabel control={<Checkbox />} label="Software de contabilidad (especificar)" />
+                        <FormControlLabel control={<Checkbox />} label="Otro (especificar)" />
+                        
+                </FormGroup>
+
+
+                </form>
+                </main>
+                </body>
                 <button className="btn-cont-reg">Siguiente</button>
-            </div>
 
-        </div>
-
-
+                </html>
 )
 }
 
